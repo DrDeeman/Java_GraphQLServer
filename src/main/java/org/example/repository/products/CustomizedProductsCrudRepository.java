a89f0f3077496078a94f7cd9688757e4b1130a28
drdeeman <stalkerdrdeeman@gmail.com>
2025-03-11 15:58:34 +0400

    добавление выборки связи для eUsers

--- src/main/java/org/example/repository/products/CustomizedProductsCrudRepository.java
+++ src/main/java/org/example/repository/products/CustomizedProductsCrudRepository.java
@@ -1,9 +1,16 @@
 package org.example.repository.products;
 
 import org.example.entity.eProducts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CustomizedProductsCrudRepository extends CrudRepository<eProducts, Integer> {
+public interface CustomizedProductsCrudRepository extends JpaRepository<eProducts, Integer> {
+
+    @Query(value = "FROM eProducts p JOIN FETCH p.user WHERE p.id=:id")
+    Optional<eProducts> findById(int id);
 }
